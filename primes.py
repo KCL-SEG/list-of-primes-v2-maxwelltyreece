4acbaee3d40e27c87338d7dfce1ab97ed7e456e5
Maxwell Martin <maxwelltymartin@gmail.com>
2023-09-26 14:52:34 +0100

    Attempt 1

--- primes.py
+++ primes.py
@@ -3,4 +3,21 @@
 
 def primes(number_of_primes):
     list = []
+    testNumber = 1
+    flag = False
+    while len(list) < number_of_primes:
+        testNumber+=1
+        flag = False
+
+        #as all numbers are comprised of prime factors, by checking our list of primes smaller then the
+        #target number, if it is divisible by one of them, it is not prime.
+        for prime in list:
+            if testNumber % prime == 0:
+                flag = True
+                break
+
+        if flag == False:
+            list.append(testNumber)            
+
     return list
+
